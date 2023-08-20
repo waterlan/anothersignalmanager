a90c224d4073f03ac0229e31f7504ee5ed4018ae
Erwin Waterlander <waterlan@xs4all.nl>
2023-08-20 18:52:05 +0200

    Fixed problem that magnitude and phase commands could not be run on time domain signals.

--- asm/src/math/Transformations.java
+++ asm/src/math/Transformations.java
@@ -236,7 +236,7 @@ public class Transformations extends MathBase {
         {
             throw new SignalDoesNotExist("Signal \"" + signalname + "\" does not exist.");
         }
-        if (signal.getDataDomain() != Signal.FREQ) {
+        if (signal.getDataDomain() != Signal.FREQ && signal.getDataDomain() != Signal.TIME) {
             throw new WrongDomain(String.format("Command \"%s\" can't operate on signal \"%s\" of domain %s.", command,
                     signalname, signal.getDataDomainToString()));
         }
@@ -342,7 +342,7 @@ public class Transformations extends MathBase {
         {
             throw new SignalDoesNotExist("Signal \"" + signalname + "\" does not exist.");
         }
-        if (signal.getDataDomain() != Signal.FREQ) {
+        if (signal.getDataDomain() != Signal.FREQ && signal.getDataDomain() != Signal.TIME) {
             throw new WrongDomain(String.format("Command \"%s\" can't operate on signal \"%s\" of domain %s.", command,
                     signalname, signal.getDataDomainToString()));
         }
